992b209a726da649222803dc900a92659ed50439
Zach Sherman <zksherm@gmail.com>
2012-10-21 20:12:26 -0500

    more style updates

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :title, :description, :comments_attributes
+  attr_accessible :title, :description, :comments_attributes, :created_at
   validates_presence_of :title, :description
   belongs_to :user
   has_many :comments, :dependent => :destroy
