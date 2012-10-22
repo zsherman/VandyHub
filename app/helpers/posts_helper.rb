9c121faee9a009fd169f51283e040814af0e40f6
Zach Sherman <zksherm@gmail.com>
2012-10-21 21:13:17 -0500

    more style changes

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,8 @@
 module PostsHelper
+
+	def post_teaser(post)
+  		output = h truncate(post.description, :length => 200, :omission => '...')
+  		output += link_to('[more]', '/posts/' + post.id.to_s) if post.description.size > 200
+  		output.html_safe
+	end
 end
