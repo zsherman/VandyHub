246623b883a9903e39ceeca4d1e38ffa2316f3de
Zach Sherman <zksherm@gmail.com>
2012-10-22 23:28:50 -0500

    adding bronelius

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',       # required
-    :aws_access_key_id      => ENV['S3_KEY'],       # required
-    :aws_secret_access_key  => ENV['S3_SECRET'],       # required
+    :aws_access_key_id      => '',       # required
+    :aws_secret_access_key  => '',       # required
     :region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'vandy-hub'
