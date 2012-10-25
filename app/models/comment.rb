7743e226678a05ddd4a53325bf7b20e7231cbee9
Zach Sherman <zksherm@gmail.com>
2012-10-25 00:43:28 -0500

    image stuff

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ActiveRecord::Base
-  attr_accessible :content, :parent_id
+  attr_accessible :content, :parent_id, :image, :remote_image_url
   validates_presence_of :content
   belongs_to :user
   belongs_to :post
   has_ancestry
+  mount_uploader :image, ImageUploader
 end
