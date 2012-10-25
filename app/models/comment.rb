5ff8f278a8575f9d3f22d1d025bd26035435461f
Zach Sherman <zksherm@gmail.com>
2012-10-25 01:03:56 -0500

    spacing stuff

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,5 @@
 class Comment < ActiveRecord::Base
   attr_accessible :content, :parent_id, :image, :remote_image_url
-  validates_presence_of :content
   belongs_to :user
   belongs_to :post
   has_ancestry
